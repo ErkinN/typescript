cc6fb96a610dcf61755c0fad1238a38510eee9a7
Erkin Abdullaev <93423206+ErkinN@users.noreply.github.com>
2022-12-26 20:41:07 +0900

    Add files via upload

---
+++ src/classes/Payment.ts
@@ -0,0 +1,18 @@
+import { HasFormatter } from "../Interfaces/HasFormatter";
+
+//Classes
+export class Payment implements HasFormatter {
+  //   readonly client: string;
+  //   private details: string;
+  //   public amount: number;
+
+  constructor(
+    readonly recipient: string,
+    private details: string,
+    public amount: number
+  ) {}
+
+  format() {
+    return `${this.recipient} is owed $${this.amount} for ${this.details}`;
+  }
+}
